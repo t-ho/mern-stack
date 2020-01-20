9c727a33289af54fb89206bdda1fcbcda81e3920
t-ho <toan.hmt@protonmail.com>
2020-01-19 22:06:18 -0800

    web: UserList page require admin or root role

--- web-client/src/containers/App/App.js
+++ web-client/src/containers/App/App.js
@@ -4,6 +4,7 @@ import Header from '../../components/Header';
 import SignIn from '../Auth/SignIn';
 import SignUp from '../Auth/SignUp';
 import Profile from '../Auth/Profile';
+import UserList from '../Users/UserList';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/signin" component={SignIn} />
         <Route path="/signup" component={SignUp} />
         <Route path="/profile" component={Profile} />
+        <Route path="/users" component={UserList} />
         <Route path="/" component={SignIn} />
         <Redirect to="/" />
       </Switch>
---
+++ web-client/src/containers/Users/UserList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import requireRole from '../../hoc/requireRole';
+
+const UserList = () => {
+  return (
+    <div>
+      <h3>UserList Page</h3>
+      Only root or admin can go here
+    </div>
+  );
+};
+
+export default requireRole(UserList, 'admin');
