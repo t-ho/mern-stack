0966bd72194c4045fb961a64d044a11c2c38d02c
t-ho <toan.hmt@protonmail.com>
2020-02-22 02:11:54 -0800

    server(test): Add test for endpoint /api/alive

---
+++ server/test/routes/api/alive.route.test.js
@@ -0,0 +1,11 @@
+const request = require('supertest');
+
+describe('ENDPOINT: GET /api/alive', function() {
+  const endpoint = '/api/alive';
+  it(`GET ${endpoint} - Health check succeeded`, function(done) {
+    request(app)
+      .get(endpoint)
+      .expect(200)
+      .expect({ status: 'pass' }, done);
+  });
+});
