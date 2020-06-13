d1b4c4fe1090505ca3a30400c31df663cdfb6dcb
t-ho <t.ho@tdev.app>
2020-06-12 19:57:57 -0700

    nginx-proxy: Use certbot to install letsencrypt certificate and harden nginx server

--- nginx-proxy/nginx-entrypoint.sh
+++ nginx-proxy/nginx-entrypoint.sh
@@ -5,6 +5,11 @@ export PORT=${PORT:-3000}
 export SERVER_PORT=${SERVER_PORT:-8861}
 export NGINX_PROXY_PORT=${NGINX_PROXY_PORT:-80}
 
-envsubst '${PORT} ${SERVER_PORT} ${NGINX_PROXY_PORT}' < /nginx.default.conf.template > /etc/nginx/conf.d/default.conf
+if [ $NODE_ENV = production ] && [ ! -f /etc/letsencrypt/ssl_dhparam.pem ]; then
+  openssl dhparam -out /etc/letsencrypt/ssl_dhparam.pem 2048
+fi
+
+
+envsubst '${DOMAIN_NAME} ${NGINX_PROXY_PORT} ${PORT} ${SERVER_PORT}' < /nginx.default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
