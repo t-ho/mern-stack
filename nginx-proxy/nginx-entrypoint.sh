8b454d165304997afc68277ede98527fc7263281
t-ho <t.ho@tdev.app>
2020-06-13 00:40:42 -0700

    nginx-proxy: Now can specify a list of domain names and public IP address

--- nginx-proxy/nginx-entrypoint.sh
+++ nginx-proxy/nginx-entrypoint.sh
@@ -1,15 +1,21 @@
 #!/usr/bin/env bash
 set -eu
 
-export PORT=${PORT:-3000}
-export SERVER_PORT=${SERVER_PORT:-8861}
 export NGINX_PROXY_PORT=${NGINX_PROXY_PORT:-80}
+export PORT=${PORT:-3000}
+export PUBLIC_IP_ADDRESS=${PUBLIC_IP_ADDRESS:-127.0.0.1}
+export SERVER_PORT=${SERVER_PORT:-8861}
 
-if [ $NODE_ENV = production ] && [ ! -f /etc/letsencrypt/ssl_dhparam.pem ]; then
-  openssl dhparam -out /etc/letsencrypt/ssl_dhparam.pem 2048
+if [ $NODE_ENV = production ]; then
+  if [ ! -f /etc/letsencrypt/ssl_dhparam.pem ]; then
+    openssl dhparam -out /etc/letsencrypt/ssl_dhparam.pem 2048
+  fi
+
+  export PRIMARY_DOMAIN=$(echo "$DOMAIN_NAMES" | cut -d "," -f 1)
+  export DOMAIN_LIST=$(echo "$DOMAIN_NAMES" | tr "," " ")
 fi
 
 
-envsubst '${DOMAIN_NAME} ${NGINX_PROXY_PORT} ${PORT} ${SERVER_PORT}' < /nginx.default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst '${DOMAIN_LIST} ${NGINX_PROXY_PORT} ${PORT} ${PRIMARY_DOMAIN} ${PUBLIC_IP_ADDRESS} ${SERVER_PORT}' < /nginx.default.conf.template > /etc/nginx/conf.d/default.conf
 
 exec "$@"
